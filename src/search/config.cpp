6c63535cc4f5a010add36ee8af5040a1fe0ef01e
Ilya Gusev <phoenixilya@gmail.com>
2022-06-29 17:40:15 +0300

    Config fixes

--- src/search/config.cpp
+++ src/search/config.cpp
@@ -16,4 +16,16 @@ void TSearchConfig::Parse(const std::string& configFile) {
     EnablePST = root.get("enable_pst", EnablePST).asBool();
     EnableTT = root.get("enable_tt", EnableTT).asBool();
     QuiescenceSearchDepth = root.get("qsearch_depth", QuiescenceSearchDepth).asInt();
+
+    EnableNullMove = root.get("enable_null_move", EnableNullMove).asBool();
+    NullMoveDepthReduction = root.get("null_move_depth_reduction", NullMoveDepthReduction).asInt();
+    NullMoveEvalMargin = root.get("null_move_eval_margin", NullMoveEvalMargin).asInt();
+
+    EnableLMR = root.get("enable_lmr", EnableLMR).asBool();
+    LMRMinMoveNumber = root.get("lmr_min_move_number", LMRMinMoveNumber).asInt();
+    LMRMinPly = root.get("lmr_min_ply", LMRMinPly).asInt();
+    LMRMinDepth = root.get("lmr_min_depth", LMRMinDepth).asInt();
+    LMRReduction = root.get("lmr_reduction", LMRReduction).asInt();
+
+    EnableKillers = root.get("enable_killers", EnableKillers).asInt();
 }
